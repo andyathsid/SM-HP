73ae8b7e6347d7ae7d68e2f858867896ecd2e55d
andyathsid <andakaraas@gmail.com>
2024-06-02 12:37:31 +0700

    Add line chart for dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Admin;
 use Cookie;
+use App\Models\Booking;
 
 class AdminController extends Controller
 {
@@ -37,5 +38,16 @@ class AdminController extends Controller
     function logout(){
         session()->forget(['adminData']);
         return redirect('admin/login');
+    }
+
+    function dashboard(){
+        $bookings=Booking::selectRaw('count(id) as total_bookings,checkin_date')->groupBy('checkin_date')->get();
+        $labels=[];
+        $data=[];
+        foreach($bookings as $booking){
+            $labels[]=$booking['checkin_date'];
+            $data[]=$booking['total_bookings'];
+        }
+        return view('dashboard',['labels'=>$labels,'data'=>$data]);
     }
 }
